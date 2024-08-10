8d15ea817acb3541434eecbea86a663c32a9185c
BRYANGC59 <bryangarciacastano@gmail.com>
2024-08-09 23:52:49 -0500

    Update CARTA.py

---
+++ ACT 2/CARTA.py
@@ -0,0 +1,12 @@
+class carta:
+    def __init__(self, valor, pinta):
+        self.valor = valor
+        self.pinta = pinta
+    
+    colores = input 
+    blanco = input
+    negro = input
+    puntillismo = input
+
+
+    
